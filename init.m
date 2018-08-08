c15b6352245cf70295af7e5d415eb17d882f8104
Doron Behar <doron.behar@gmail.com>
2018-08-08 07:23:14 +0300

    Move all packages to dedicated directory.
    Put submodules in packages/bundle/ and builds and installations (like
    from `make install`) in packages/build.

--- init.m
+++ init.m
@@ -1,1 +1,3 @@
-addpath("~/.config/octave/matlab2tikz/src")
+addpath("/var/code/doron/dotfiles/.config/octave/packages/bundle/matlab2tikz/src");
+pkg prefix /var/code/doron/dotfiles/.config/octave/packages/build /var/code/doron/dotfiles/.config/octave/packages/build;
+pkg local_list /var/code/doron/dotfiles/.config/octave/packages/index;
