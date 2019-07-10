d687014368a61546fc6bc9318d391e73f2e5a08e
Doron Behar <doron.behar@gmail.com>
2019-07-10 13:28:50 +0300

    Use relative, normal paths for packages

--- init.m
+++ init.m
@@ -1,3 +1,3 @@
-addpath("/var/code/doron/dotfiles/.config/octave/packages/bundle/matlab2tikz/src");
-pkg prefix /var/code/doron/dotfiles/.config/octave/packages/build /var/code/doron/dotfiles/.config/octave/packages/build;
-pkg local_list /var/code/doron/dotfiles/.config/octave/packages/index;
+addpath("~/.config/octave/packages/bundle/matlab2tikz/src");
+pkg prefix ~/.config/octave/packages/build ~/.config/octave/packages/build;
+pkg local_list ~/.config/octave/packages/index;
