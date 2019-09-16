efcacbd7381f5093078d1fe573679fe8b2e57adf
Doron Behar <doron.behar@gmail.com>
2019-09-16 19:34:59 +0300

    Remove packages index not ignored from repo and init.m

--- init.m
+++ init.m
@@ -1,3 +1,3 @@
 addpath("~/.config/octave/packages/bundle/matlab2tikz/src");
 pkg prefix ~/.config/octave/packages/build ~/.config/octave/packages/build;
-pkg local_list ~/.config/octave/packages/index;
+pkg load symbolic
