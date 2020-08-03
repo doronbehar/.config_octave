1009508832421fabde16c95512b03936581ac72d
Doron Behar <doron.behar@gmail.com>
2020-08-03 19:55:57 +0300

    Add latexTable script

--- init.m
+++ init.m
@@ -1,3 +1,4 @@
 addpath("~/.config/octave/packages/bundle/matlab2tikz/src");
+addpath("~/.config/octave/packages/bundle/latexTable")
 pkg prefix ~/.config/octave/packages/build ~/.config/octave/packages/build;
 pkg load symbolic
