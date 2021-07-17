7a241af155d70880a9d3941f29413ecd2ceeeb36
Doron Behar <doron.behar@gmail.com>
2021-07-17 09:43:51 +0300

    Load symbolic package via octave.withPackages

--- init.m
+++ init.m
@@ -1,4 +1,2 @@
 addpath("~/.config/octave/packages/bundle/matlab2tikz/src");
 addpath("~/.config/octave/packages/bundle/latexTable")
-pkg prefix ~/.config/octave/packages/build ~/.config/octave/packages/build;
-pkg load symbolic
